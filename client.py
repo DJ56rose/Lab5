cad967905bf7a28b96643a8398dea3f3fb462f5c
blahblah <amanda720.rose@gmail.com>
2019-03-11 18:48:31 -0600

    As of Monday at 7pm

--- client.py
+++ client.py
@@ -1,7 +1,9 @@
-import socket, sys
+import socket, sys, os
 
 # accept server IP, file
-
+if (len(sys.argv) != 3):
+	print("ERROR: wrong number of arguments")
+	exit()
 IP = sys.argv[1]
 filename = sys.argv[2]
 
@@ -10,21 +12,38 @@ try: fn = open(filename,"rb")
 except:
     print("ERROR: invalid filename")
     exit()
-
 IPnums = IP.split('.')
 if len(IPnums) != 4:
     print("ERROR: Incorrect IP format")
     exit()
 
+# initialize socket
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+port = 12346
+host = socket.gethostname()
+print(host)
+#connect
+s.connect((host,port))
+filesize = os.path.getsize(filename)
+print("FROM CLIENT: sending "+str(filesize)+" bytes")
+
+# send filename
+filename = bytearray(filename,'utf-8')
+s.sendall(filename)
+
 # === loop through file ===
 X = 1024
-data = ""
 info = fn.read(X)
+data = info	#for tracking
 while info:
-    # send over link
-    
-    # read again
-    info = fn.read(X)
-    print(info)
-    #print(info)
-print(data)
+	# send over link
+	s.sendall(info)	
+	# read again
+	info = fn.read(X)
+	data += info
+
+# receive confirmation
+msg = "FROM SERVER: " + str(s.recv(1024))
+print(msg)
+s.close()
+#print(data)
--- server.py
+++ server.py
@@ -1,22 +1,40 @@
 import socket
 
-s = socket.socket()
+server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
 #bind
 host = socket.gethostname()
-port = 12345
-s.bind((host,port))
-
+ip = socket.gethostbyname(host)
+port = 12346
+server.bind((host,port))
+print(host)
 #listen
-s.listen(5)
+server.listen(5)
 
 while True:
     # accept
-    c, addr = s.accept()
-
-
-    #read
+	client, addr = server.accept()
+	
+	# read & create filename
+	filename = client.recv(1024)
+	filename = filename.decode('utf-8') + "_server"
+	fn = open(filename,'wb')
+	#print("Just opened stuff...")	
+    # read data
+	X = 1024
+	while True:
+		data = client.recv(X)
+		fn.write(data)
+		print(len(data))
+		if (len(data) < 1024):
+			
+			X = len(data)
+			print("Breaking...")
+			break
 
     #write
-
+	confirm = "Received all " + str(len(data)) + " bytes"
+	tosend = bytearray(confirm,'utf-8')
+	client.send(tosend)
     #close
+	client.close()
